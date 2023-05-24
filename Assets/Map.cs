790b4c12c7535d664328c3e67166b75637172c91
Yoann Ochietti <yoann.ochietti@gmail.com>
2023-05-24 05:21:47 -0400

    grid made to start from bottom left of screen and support flatTop and pointyTop modes with any x and y dimensions

--- Assets/Map.cs
+++ Assets/Map.cs
@@ -7,19 +7,19 @@ public class Map : MonoBehaviour
     //sets orientation of hexes
     public bool isFlatTop = true;
 
-    //cuts off hexes beyond flatSize diameter
-    public bool isCircular= true;
-
-    //size of tallest column in flatTop orientation
-    public int flatSize = 7;
+    public int xSize = 50;
+    public int ySize = 50;
 
     public Hex hexPrefab;
 
-    //point to point, or width (two times side length)
+    //corner to corner, or width (two times side length)
+    //should correspond to unscaled sprite size
     public float hexWidth = 1f;
 
     // flat to flat, or height, calculated below by constant and width
     private float hexHeight;
+
+    public float padding = 0.1f;
 
     private Hex[,] hexGrid;
 
@@ -27,40 +27,41 @@ public class Map : MonoBehaviour
     void Start()
     {
 
-        bool oddFlatSize = flatSize % 2 == 1 ? true : false;
+        hexGrid = new Hex[xSize, ySize];
 
-        if (!oddFlatSize)
-        {
-            Debug.LogError("Only odd flatSize is currently supported");
-        }
-
-        if (!isFlatTop)
-        {
-            Debug.LogError("Only flatTop is currently supported");
-        }
-
-        hexGrid = new Hex[flatSize*2,flatSize];
-        //adds a bit of padding between hexes
-        hexWidth += 0.1f;
+        //not sure about this approach, but it works for now..
+        hexWidth += padding;
         hexHeight = hexWidth / 1.155f;
 
-        int startingX = isCircular ? -(flatSize / 2) : -(flatSize - 1);
-        int endingX = -startingX;
-        for (int x = startingX; x <= endingX; x++)
+        for (int x = 0; x < xSize; x++)
         {
-            int colSize = flatSize - Mathf.Abs(x);
-            int startingY = -(colSize - 1) / 2;
-            int endingY = colSize + startingY -1;
-            for (int y = startingY; y <= endingY; y++)
+            for (int y = 0; y < ySize; y++)
             {
-                float xPos = (3 * hexWidth / 4) * x;
-                float yPos = Mathf.Abs(x) % 2 == 1 ? (y * hexHeight) - (hexHeight / 2) : (y * hexHeight);
-                Hex hex = (Hex)Instantiate(hexPrefab, new Vector3(xPos, yPos, 0f), Quaternion.identity);
+                float xPos;
+                if (isFlatTop)
+                {
+                    xPos =  x * (3f * hexWidth / 4.0f);
+                } else {
+                    xPos = y % 2 == 0 ? x * hexHeight : x * hexHeight + hexHeight / 2f;
+                }
+                xPos += Camera.main.ViewportToWorldPoint(Vector3.zero).x;
+
+                float yPos;
+                if (isFlatTop)
+                {
+                    yPos = x % 2 == 0 ? y * hexHeight : y * hexHeight + hexHeight / 2f;
+                }
+                else
+                {
+                    yPos = y * (3f * hexWidth / 4.0f);
+                }
+                yPos += Camera.main.ViewportToWorldPoint(Vector3.zero).y;
+
+                Hex hex = (Hex)Instantiate(hexPrefab, new Vector3(xPos, yPos, 0), isFlatTop ? Quaternion.identity : Quaternion.AngleAxis(90, new Vector3(0,0,1)));
                 hex.name = "Hex_" + x + "_" + y;
+                hexGrid[x, y] = hex;
             }
         }
-
-
     }
 
     // Update is called once per frame
