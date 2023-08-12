9e51c3a9cd546c29ae2cda241c3e0838a6ae7418
Yoann Ochietti <yoann.ochietti@gmail.com>
2023-08-12 19:07:06 -0400

    Whitened all sprites and did some prefab coloring.

---
+++ tools/white_wash_images.ps1
@@ -0,0 +1,23 @@
+
+# Specify the root folder
+$rootFolder = "E:\code\temp_name\thom\Assets\Art\2D"
+
+# Specify the path to magick.exe
+$magickPath = "magick.exe"
+
+# Create a 'white' folder within the root folder
+$whiteFolder = Join-Path $rootFolder "white"
+if (-not (Test-Path -Path $whiteFolder)) {
+    New-Item -Path $whiteFolder -ItemType Directory
+}
+
+# Get all PNG files in subfolders
+$pngFiles = Get-ChildItem -Path $rootFolder -Recurse -Filter "*.png" | Where-Object { $_.Extension -eq ".png" }
+
+# Loop through each PNG file and run the magick command
+foreach ($pngFile in $pngFiles) {
+    $outputFile = Join-Path $whiteFolder $pngFile.Name
+    $command = "$magickPath `"$($pngFile.FullName)`" -fill white -colorize 100% `"$outputFile`""
+    Invoke-Expression $command
+    Write-Host "Processed: $($pngFile.FullName) => $($outputFile)"
+}
