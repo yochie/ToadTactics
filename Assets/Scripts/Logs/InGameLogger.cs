5bf3133cad0a4b51ed623ee91bff7ecd8dc3c54e
Yoann Ochietti <yoann.ochietti@gmail.com>
2023-08-26 09:22:46 -0400

    Log window scrolling fixed using bunch of approaches combined. Messy but it works...

--- Assets/Scripts/Logs/InGameLogger.cs
+++ Assets/Scripts/Logs/InGameLogger.cs
@@ -15,6 +15,9 @@ public class InGameLogger : NetworkBehaviour, ILogger
     [SerializeField]
     private Scrollbar scrollbar;
 
+    [SerializeField]
+    private ScrollRect scrollRect;
+
     private void Awake()
     {
         Debug.Log("Awaking InGameLogger");
@@ -34,15 +37,55 @@ public class InGameLogger : NetworkBehaviour, ILogger
     {
         LogEntry logEntry = Instantiate(this.logEntryPrefab, this.logListOnScreen.transform);
         logEntry.SetText(message);
-        this.ScrollToBottom();
+        StartCoroutine(this.ScrollToBottom());
     }
 
-    //NOT WORKING
-    //TODO : FIX
-    private void ScrollToBottom()
+
+    private IEnumerator ScrollToBottom()
     {
+        //Throwing everything ive got at this fking mess
+        //currently working but its unclear if all this is required, would need more testing to cleanup
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForEndOfFrame();
+        this.UpdateLayout(this.transform);
         this.scrollbar.value = 0;
+        this.scrollRect.verticalNormalizedPosition = 0;
+        LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)this.scrollRect.transform);
+        Canvas.ForceUpdateCanvases();
     }
 
+    //https://forum.unity.com/threads/scroll-to-the-bottom-of-a-scrollrect-in-code.310919/
+    private void UpdateLayout(Transform transform)
+    {
+        Canvas.ForceUpdateCanvases();
+        LayoutRebuilder.ForceRebuildLayoutImmediate((RectTransform)this.scrollRect.transform);
+        UpdateLayout_Internal(this.transform.parent);
+    }
+
+    private void UpdateLayout_Internal(Transform transform)
+    {
+        if (transform == null || transform.Equals(null))
+        {
+            return;
+        }
+
+        // Update children first
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            UpdateLayout_Internal(transform.GetChild(i));
+        }
+
+        // Update any components that might resize UI elements
+        foreach (var layout in transform.GetComponents<LayoutGroup>())
+        {
+            layout.CalculateLayoutInputVertical();
+            layout.CalculateLayoutInputHorizontal();
+        }
+        foreach (var fitter in transform.GetComponents<ContentSizeFitter>())
+        {
+            fitter.SetLayoutVertical();
+            fitter.SetLayoutHorizontal();
+        }
+    }
 
 }
