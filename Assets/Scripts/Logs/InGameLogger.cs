479ba4688ed9c831976b4ccfec5286dd76d70222
Yoann Ochietti <yoann.ochietti@gmail.com>
2023-08-25 21:09:52 -0400

    Log window adjustments

--- Assets/Scripts/Logs/InGameLogger.cs
+++ Assets/Scripts/Logs/InGameLogger.cs
@@ -2,9 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.UI;
 
 public class InGameLogger : NetworkBehaviour, ILogger
 {
+    [SerializeField]
+    private GameObject logListOnScreen;
+
+    [SerializeField]
+    private LogEntry logEntryPrefab;
+
+    [SerializeField]
+    private Scrollbar scrollbar;
 
     private void Awake()
     {
@@ -21,18 +30,19 @@ public class InGameLogger : NetworkBehaviour, ILogger
         MasterLogger.Singleton.RemoveLogger(this);
     }
 
-    [SerializeField]
-    private GameObject logListOnScreen;
-
-    [SerializeField]
-    private LogEntry logEntryPrefab;
-
     public void LogMessage(string message)
     {
         LogEntry logEntry = Instantiate(this.logEntryPrefab, this.logListOnScreen.transform);
         logEntry.SetText(message);
+        this.ScrollToBottom();
     }
 
+    //NOT WORKING
+    //TODO : FIX
+    private void ScrollToBottom()
+    {
+        this.scrollbar.value = 0;
+    }
 
 
 }
