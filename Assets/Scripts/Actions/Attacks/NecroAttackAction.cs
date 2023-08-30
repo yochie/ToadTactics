f81dba22ad5cfc318c5a02521714d6a66c627d61
Yoann Ochietti <yoann.ochietti@gmail.com>
2023-08-29 21:59:28 -0400

    Necro attack action added.

---
+++ Assets/Scripts/Actions/Attacks/NecroAttackAction.cs
@@ -0,0 +1,27 @@
+﻿using Mirror;
+
+internal class NecroAttackAction : DefaultAttackAction
+{
+    const int NECRO_SELF_DAMAGE = 10;
+    const DamageType NECRO_SELF_DAMAGE_TYPE = DamageType.magic;
+
+    [Server]
+    public override void ServerUse(INetworkedLogger logger)
+    {
+        base.ServerUse(logger);
+        ActionExecutor.Singleton.CustomAttack(source: this.ActorHex,
+                                              primaryTarget: this.ActorHex,
+                                              areaType: AreaType.single,
+                                              areaScaler: 1,
+                                              damage: NECRO_SELF_DAMAGE,
+                                              damageType: NECRO_SELF_DAMAGE_TYPE,
+                                              damageIterations: 1,
+                                              penetratingDamage: false,
+                                              knocksBack: false,
+                                              canCrit: false,
+                                              critChance: 0,
+                                              critMultiplier: 0, 
+                                              sender: this.RequestingClient);
+    }
+
+}
