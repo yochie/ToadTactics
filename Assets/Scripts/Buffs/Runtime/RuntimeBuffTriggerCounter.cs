7296b0c22a98ead571f6fcfb8be3a300c7e0587b
Yoann Ochietti <yoann.ochietti@gmail.com>
2023-09-02 19:13:25 -0400

    Missing files added

---
+++ Assets/Scripts/Buffs/Runtime/RuntimeBuffTriggerCounter.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RuntimeBuffTriggerCounter : IRuntimeBuffComponent
+{
+    public int RemainingTriggers{ get; set; }
+
+}
