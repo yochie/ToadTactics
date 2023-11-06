09435922e7a8292ae069c459bdd86172216f670f
Yoann Ochietti <yoann.ochietti@gmail.com>
2023-11-06 17:16:29 -0500

    Attack slash effect positioning fixed.

--- Assets/AttackEffectSpawner.cs
+++ Assets/AttackEffectSpawner.cs
@@ -13,11 +13,17 @@ public class AttackEffectSpawner : MonoBehaviour
     [SerializeField]
     private PlayerCharacter forCharacter;
 
+    [SerializeField]
+    private Vector3 spawnOffsets;
+
+    [SerializeField]
+    private float spawnRotation;
+
     public void OnCharacterAttacks(int classID)
     {
         if (classID != forCharacter.CharClassID)
             return;
-        Instantiate(effectAnimation, this.spawnLocation.position, Quaternion.AngleAxis(180, Vector3.forward));
+        Instantiate(effectAnimation, this.spawnLocation.position + spawnOffsets, Quaternion.AngleAxis(spawnRotation, Vector3.forward), forCharacter.transform);
     }
 
 }
