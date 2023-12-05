fc296763ac50f69296705be7252e82055724895e
Yoann Ochietti <yoann.ochietti@gmail.com>
2023-12-05 13:24:57 -0500

    Added sound effect for ability usage. Restored effect destruction that was removed for debugging. MAKE SURE YOU REMOVE ANIMATION TRANSITION EXIT TIMES.

--- Assets/Scripts/VFX/AnimationEffect.cs
+++ Assets/Scripts/VFX/AnimationEffect.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class AnimationEffect : MonoBehaviour
 {
     private bool isRunning;    
+
     [SerializeField]
     private Animator animator;
+
+    [SerializeField]
+    private AudioClip soundEffect;
 
     public void DestroyMe()
     {
         this.isRunning = false;
-        //Destroy(gameObject);
+        Destroy(gameObject);
     }
 
     public IEnumerator RunAnimation()
     {
         this.isRunning = true;
         this.animator.SetBool("Started", true);
+        if(this.soundEffect != null)
+        {
+            AudioManager.Singleton.PlaySoundEffect(this.soundEffect);
+        }
         while (this.isRunning)
         {
             yield return null;
         }
     }
-
 }
