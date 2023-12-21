28ce8ac3b6e5608b2e4e60daf264a5455b349af9
Yoann Ochietti <yoann.ochietti@gmail.com>
2023-12-21 17:55:01 -0500

    Round end screens can now be hidden for reviewing game after it has ended. Also made semi transparent by default.

--- Assets/Scripts/UI/RoundEndScreen.cs
+++ Assets/Scripts/UI/RoundEndScreen.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RoundEndScreen : MonoBehaviour
 {
@@ -13,7 +14,29 @@ public class RoundEndScreen : MonoBehaviour
     private TextMeshProUGUI scoreLabel;
 
     [SerializeField]
-    private TextMeshProUGUI buttonText;
+    private TextMeshProUGUI continueButtonText;
+
+    [SerializeField]
+    private Button hideButton;
+
+    [SerializeField]
+    private TextMeshProUGUI hideButtonText;
+
+    [SerializeField]
+    private GameObject textContent;
+
+    private bool hidden;
+
+    private Color baseColor;
+
+    private float baseAlpha;
+
+    private void Awake()
+    {
+        this.hidden = false;
+        this.baseColor = this.container.GetComponent<Image>().color;
+        this.baseAlpha = this.baseColor.a;
+    }
 
     public void ActivateMyself()
     {
@@ -22,6 +45,9 @@ public class RoundEndScreen : MonoBehaviour
         int opponentScore = GameController.Singleton.GetScore(GameController.Singleton.NonLocalPlayer.playerID);
         this.scoreLabel.text = string.Format("You : {0}\nOpponent : {1}", yourScore, opponentScore);
         this.container.SetActive(true);
+        this.textContent.SetActive(true);
+        this.hidden = false;
+        this.hideButton.gameObject.SetActive(true);
     }
 
     public void ReturnToMainMenu()
@@ -36,8 +62,16 @@ public class RoundEndScreen : MonoBehaviour
     {
         if (!GameController.Singleton.isServer)
             //TODO : make it so both players have to click button, not just host
-            this.buttonText.text = "Waiting for host to proceed";
+            this.continueButtonText.text = "Waiting for host to proceed";
         else
             GameController.Singleton.CmdChangeToScene("EquipmentDraft");
+    }
+
+    public void ToggleHideScreen()
+    {
+        this.hidden = !this.hidden;
+        this.container.GetComponent<Image>().color = Utility.SetAlpha(this.baseColor, this.hidden ? 0 : this.baseAlpha);
+        this.textContent.SetActive(!this.hidden);
+        this.hideButtonText.text = this.hidden ? "Return" : "View game";
     }
 }
