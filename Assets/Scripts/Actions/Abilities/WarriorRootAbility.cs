5e163c9d498a3544c621b6c1f21ccd05fd33d370
Yoann Ochietti <yoann.ochietti@gmail.com>
2024-01-19 17:23:32 -0500

    Knockback support added to warrior root ability.

--- Assets/Scripts/Actions/Abilities/WarriorRootAbility.cs
+++ Assets/Scripts/Actions/Abilities/WarriorRootAbility.cs
@@ -40,11 +40,56 @@ public class WarriorRootAbility : IAbilityAction, ITargetedAction, IActivatedBuf
         this.ActorCharacter.UsedAbility(this.AbilityStats.stringID);
 
         List<Hex> targetedHexes = AreaGenerator.GetHexesInArea(Map.Singleton.hexGrid, this.TargetedAreaType, this.ActorHex, this.TargetHex, this.AreaScaler);
-        List<int> affectedCharacters = targetedHexes
+
+        //filter out friendly characters
+        List<PlayerCharacter> affectedCharacters = targetedHexes
             .Where(hex => hex.HoldsACharacter() && hex.GetHeldCharacterObject().OwnerID != this.RequestingPlayerID)
-            .Select(hex => hex.holdsCharacterWithClassID).ToList();
-        RuntimeBuff buff = BuffManager.Singleton.CreateAbilityBuff(this.AppliesBuffOnActivation, this.AbilityStats, this.ActorCharacter.CharClassID, affectedCharacters);
+            .Select(hex => hex.GetHeldCharacterObject()).ToList();
+        List<int> affectedCharacterIDs = affectedCharacters.Select(character => character.CharClassID).ToList();
+        RuntimeBuff buff = BuffManager.Singleton.CreateAbilityBuff(this.AppliesBuffOnActivation, this.AbilityStats, this.ActorCharacter.CharClassID, affectedCharacterIDs);
         BuffManager.Singleton.ApplyNewBuff(buff);
+
+        foreach (Hex targetedHex in targetedHexes)
+        {
+            if (!targetedHex.HoldsACharacter() || targetedHex.GetHeldCharacterObject().OwnerID == this.RequestingPlayerID)
+                continue;
+
+            PlayerCharacter affectedCharacter = targetedHex.GetHeldCharacterObject();
+            if (affectedCharacter == null)
+                continue;
+
+            if (this.AbilityStats.knockback > 0 && !affectedCharacter.IsDead)
+            {
+                HexCoordinates sourceToTarget = HexCoordinates.Substract(targetedHex.coordinates, this.ActorHex.coordinates);
+                if (!sourceToTarget.OnSingleAxis())
+                    continue;
+
+                Hex knockbackDestination = MapPathfinder.KnockbackAlongAxis(Map.Singleton.hexGrid, this.ActorHex, targetedHex, knockbackDistance: this.AbilityStats.knockback);
+                bool knockbackSuccess;
+                if (knockbackDestination != null)
+                    knockbackSuccess = ActionExecutor.Singleton.CustomMove(targetedHex, knockbackDestination, this.RequestingClient);
+                else
+                    knockbackSuccess = false;
+
+                string knockbackMessage;
+                if (knockbackSuccess)
+                {
+                    knockbackMessage = string.Format("{0}'s <b>{1}</b> <color={3}><b>knockbacked</b></color> {2}",
+                        this.ActorCharacter.charClass.name,
+                        this.AbilityStats.interfaceName,
+                        affectedCharacter.charClass.name,
+                        Utility.DamageTypeToColorName(DamageType.physical));
+                }
+                else
+                {
+                    knockbackMessage = string.Format("{0}'s <b>{1}</b> attempted to knockback {2} but was blocked",
+                        this.ActorCharacter.charClass.name,
+                        this.AbilityStats.interfaceName,
+                        affectedCharacter.charClass.name);
+                }
+                logger.RpcLogMessage(knockbackMessage);
+            }
+        }
     }
 
     [Server]
