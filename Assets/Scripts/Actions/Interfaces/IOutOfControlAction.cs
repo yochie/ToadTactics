d912ad812cc75ce37bdb737a1d58d50c4be19ddd
Yoann Ochietti <yoann.ochietti@gmail.com>
2024-01-29 16:45:20 -0500

    Commenting

--- Assets/Scripts/Actions/Interfaces/IOutOfControlAction.cs
+++ Assets/Scripts/Actions/Interfaces/IOutOfControlAction.cs
@@ -1,3 +1,4 @@
-﻿internal interface IOutOfControlAction : IAction
+﻿//Allows actions to be performed by characters outside of their turn and without their owner as requester (e.g. knockback movement)
+internal interface IOutOfControlAction : IAction
 {
 }
